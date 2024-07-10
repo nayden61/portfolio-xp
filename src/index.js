c45df9ead4820b4237ee672a5e7f73eadb88de66
Nayden <nayden61@gmail.com>
2024-07-10 01:57:31 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const Component = <h1>lol</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(Component);
+
