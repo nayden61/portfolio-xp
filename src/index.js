9be41fb70fed0cfba8d339a6bf468040427f785f
Nayden <nayden61@gmail.com>
2024-07-10 03:02:17 +0100

    Added Tailwind

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const Component = <h1>lol</h1>
+function App() {
+    return(
+        <div className="bg-slate-800">
+            {Component}
+        </div>
+    )
+}
+const Component = <h1 className="text-center font-bold text-black">lol</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Component);
+root.render(<App />);
 
